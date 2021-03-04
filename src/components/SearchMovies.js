790a34e5fff16f9f497bbf9ee72744c27eae4b00
Manuel Duran <durbonca@gmail.com>
2021-03-04 16:23:11 -0300

    display info to the user

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -15,11 +15,10 @@ export default function SearchMovies(){
         .then(Response => Response.json())
         .then(data => {setMovies(data.results)})
         .catch(error=> console.error(error))
-        
-        console.log(movies)
     }
  
     return (
+        <>
         <form className="form" onSubmit={searchMovies}>
             <label 
                 className="label" 
@@ -41,5 +40,21 @@ export default function SearchMovies(){
                         Search
                 </button>
         </form>
+
+        <div className="card-list">
+            {movies.filter(movie => movie.poster_path).map(movie=> (
+                <div className="card" key={movie.id}>
+                    <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} className="card--image" alt={movie.title + ' poster'} />
+                    <div className="card--content">
+                        <h3 className="card-title">{movie.title}</h3>
+                        <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                        <p><small>RATING: {movie.vote_average}</small></p>
+                        <p className="card--desc">{movie.overview}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+
+        </>
     )
 }
