46ed0771b8a50719b96dd1cfb465453837be0cfe
Manuel Duran <durbonca@gmail.com>
2021-03-05 09:24:31 -0300

    random word search created

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -3,15 +3,18 @@ import MovieCard from "./MovieCard"
 
 export default function SearchMovies(){
 
+    const randomWords = require('random-words');
+
     const [query, setQuery] = useState('')
     const [movies, setMovies] = useState([])
     
+    const getRandomWord = async () => {
+        setQuery(randomWords())
+    }
+
     const searchMovies = async (event) => {
         event.preventDefault()
-
-
         const url = `https://api.themoviedb.org/3/search/movie?api_key=e2d0b7f154975768708e01ba3e8d3e2b&language=es-ES&query=${query}&page=1&include_adult=false`;
-        
         fetch(url)
         .then(Response => Response.json())
         .then(data => {setMovies(data.results)})
@@ -32,16 +35,23 @@ export default function SearchMovies(){
                     name="query" 
                     type="text"
                     value={query}
+                    required
                     onChange={(e)=> setQuery(e.target.value) }
                     placeholder="search movie for title"/>
-
+    
                 <button 
                     type="submit" 
                     className="button">
                         Search
                 </button>
+                <button 
+                    type="submit"
+                    onClick={getRandomWord} 
+                    className="button">
+                        Am Going to get lucky!
+                </button>
         </form>
-
+        
         <div className="card--list">
             {movies.filter(movie => movie.poster_path).map(movie=> (
                 <MovieCard movie={movie} key={movie.id}/>
